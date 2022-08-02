a163aad8ba4d118a31e9e95bd6b68253071ec2d2
TrickyDeepanshu <110325736+TrickyDeepanshu@users.noreply.github.com>
2022-08-02 21:47:49 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -55,7 +55,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                    text="☃️ᴄʜᴀɴɴᴇʟ⛄", url=f"{config.UPSTREAM_REPO}"
+                    text="☃️ᴄʜᴀɴɴᴇʟ⛄", url=f"https://t.me/DEEPANSHU_WORLD"
                 )
         ],
      ]
